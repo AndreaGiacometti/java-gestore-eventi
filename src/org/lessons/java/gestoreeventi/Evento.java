b28c25cf4c77e6b44108a8c820b255d94dc845d3
AndreaGiacometti <159144064+AndreaGiacometti@users.noreply.github.com>
2024-05-20 19:24:09 +0200

    Creata classe evento con attributi e costruttore

--- src/org/lessons/java/gestoreeventi/Evento.java
+++ src/org/lessons/java/gestoreeventi/Evento.java
@@ -1,5 +1,48 @@
 package org.lessons.java.gestoreeventi;
+import java.time.LocalDate;
 
 public class Evento {
-
+	private String titolo;
+	private LocalDate data;
+	private int postiTotali;
+	private int postiPrenotati;
+	
+	public Evento(String titolo, LocalDate data, int postiTotali) {
+		this.setTitolo(titolo);
+		this.setData(data);
+		this.setPostiTotali(postiTotali);
+		this.postiPrenotati = 0;
+	}
+	
+	public String getTitolo() {
+		return this.titolo;
+	}
+	public void setTitolo(String titolo) {
+		this.titolo = titolo;
+	}
+	
+	public LocalDate getData() {
+		return this.data;
+	}
+	public void setData(LocalDate data) {
+		if (data.isBefore(LocalDate.now())) {
+            throw new IllegalArgumentException("L'evento deve svolgersi in una data futura");
+        }
+		this.data = data;
+	}
+	
+	public int getPostiTotali() {
+		return this.postiTotali;
+	}
+	public void setPostiTotali(int postiTotali) {
+		if (postiTotali <=0) {
+			throw new IllegalArgumentException("I posti totali devono essere maggiori di 0");
+		}
+		this.postiTotali = postiTotali;
+	}
+	
+	public int getPostiPrenotati() {
+		return this.postiPrenotati;
+	}
 }
+
